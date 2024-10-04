85ff08d3a522006a48a5afae33f90e4973ac69a6
Satriyaind <indrasattriya@gmail.com>
2024-10-04 17:28:35 +0700

    Update cetak_bukti_kongres.blade.php

--- resources/views/cetak_bukti_kongres.blade.php
+++ resources/views/cetak_bukti_kongres.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Submission Result</title>
+    <title>Form Submission Resul</title>
     <!-- Include your CSS stylesheets here -->
 </head>
 <body>
