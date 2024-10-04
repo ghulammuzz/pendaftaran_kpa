c5c70581dc604261b12de0c7747bf5e1e7c9a11c
Ghulam Muzafar <88301005+ghulammuzz@users.noreply.github.com>
2024-10-04 18:57:28 +0700

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
+   .styles([
+       'resources/css/app.css',
+       // Tambahkan file CSS lainnya jika diperlukan
+   ], 'public/css/app.css')
    .options({
        processCssUrls: false,
    })
